d38b510c72666109329dca0d293fe42564a14ae1
Kavindu Santhusha <ksanthusha20@gmail.com>
2024-07-04 04:54:30 +0530

    ContactUs page done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <section>
+      <div className='px-4 mx-auto max-w-screen-md'>
+        <h2 className='heading text-center'>
+          Contact Us
+        </h2>
+        <p className='mb-8 lg:mb-16 font-light text-center text_para'>
+          Want to send feedback? Let us know.
+        </p>
+        <form action='#' className='space-y-8'>
+          <div>
+            <label htmlFor='email' className='form_label'>
+              Your Email
+            </label>
+            <input
+              type="email"
+              id='email'
+              placeholder='yourmail@gmail.com'
+              className='form_input mt-1'>
+            </input>
+            
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
 
