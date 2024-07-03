012aa52e652b82458e1db794e428ed84a256d609
Kavindu Santhusha <ksanthusha20@gmail.com>
2024-07-03 20:12:41 +0530

    side panel added

---
+++ frontend/src/pages/Beauticians/SidePanel.jsx
@@ -0,0 +1,59 @@
+
+
+const SidePanel = () => {
+  return (
+    <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
+      <div className='flex items-center justify-between'>
+        <p className='text_para mt-0 font-semibold'>Booking Price</p>
+        <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
+            1000 LKR
+        </span>  
+      </div>
+      <div className='mt-[30px]'>
+        <p className='text_para mt-0 font-semibold text-headingColor'>
+            Available Times:    
+        </p>
+
+        <ul className='mt-3'>
+            <li className='flex items-center justify-between mb-2'>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    Monday
+                </p>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    10:00 AM - 05:00 PM
+                </p>
+            </li>
+            <li className='flex items-center justify-between mb-2'>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    Tuesday
+                </p>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    10:00 AM - 05:00 PM
+                </p>
+            </li>
+            <li className='flex items-center justify-between mb-2'>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    Wednesday
+                </p>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    10:00 AM - 05:00 PM
+                </p>
+            </li>
+            <li className='flex items-center justify-between mb-2'>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    Thursday
+                </p>
+                <p className='text-[15px] leading-6 text-textColor font-semibold'>
+                    10:00 AM - 05:00 PM
+                </p>
+            </li>
+        </ul>
+      </div>
+      <button className='btn px-2 w-full rounded-md'>
+        Book Appoinment
+      </button>
+    </div>
+  )
+}
+
+export default SidePanel
