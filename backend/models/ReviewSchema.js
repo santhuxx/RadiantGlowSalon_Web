1856613eb204de3aba6b1d362c08ac8762b14e05
Kavindu Santhusha <ksanthusha20@gmail.com>
2024-07-07 22:38:31 +0530

    CRUD(reviews)

--- backend/models/ReviewSchema.js
+++ backend/models/ReviewSchema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import BeauticianSchema from "./BeauticianSchema.js";
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -24,5 +25,40 @@ const reviewSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+reviewSchema.pre(/^find/, function(next){
+
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  })
+
+  next();
+})
+
+reviewSchema.statics.calcAverageRatings = async function (beauticianId){
+
+  const stats = await this.aggregate([{
+    $match:{beautician:beauticianId},
+  },
+{
+  $group: {
+    _id: '$beautician',
+    numOfRating: {$sum:1},
+    avgRating: {$avg: '$rating'}
+    
+  }
+}
+])
+
+ await Beautician.findByIdAndUpdate(beauticianId,{
+  totalRating: stats[0].numOfRating,
+  averageRating: stats[0].avgRating,
+ })
+}
+
+reviewSchema.post("save", function(){
+  this.constructor.calcAverageRatings(this.beautician);
+})
 
 export default mongoose.model("Review", reviewSchema);
